632aec11f970d25d1ca77af77156afa5054f1c08
jdm79 <james.malvern@gmail.com>
2020-05-21 16:28:47 +0100

    add more newspapers to the list

--- app.py
+++ app.py
@@ -23,8 +23,16 @@ urls = {
   "dailymirror_url":"https://www.mirror.co.uk/",
   "dailyexpress_url": "https://www.express.co.uk/",
   "independent_url": "https://www.independent.co.uk/",
-  "financialtimes_url": "https://www.ft.com/"
-
+  "financialtimes_url": "https://www.ft.com/",
+  "metro_url": "https://metro.co.uk/",
+  "dailystar_url": "https://www.dailystar.co.uk/",
+  "inews_url": "https://inews.co.uk/",
+  "sun_url": "https://www.thesun.co.uk/",
+  "morningstar_url": "https://morningstaronline.co.uk/",
+  "eveningstandard_url": "https://www.standard.co.uk/",
+  "irishsun_url": "https://www.thesun.ie/",
+  "thescotsman_url": "https://www.scotsman.com/",
+  "irishtimes_url": "https://www.irishtimes.com/"
 }
 
 guardian_url = "https://www.theguardian.com/uk"
@@ -35,15 +43,16 @@ dailymirror_url = "https://www.mirror.co.uk/"
 dailyexpress_url = "https://www.express.co.uk/"
 independent_url = "https://www.independent.co.uk/"
 financialtimes_url = "https://www.ft.com/"
-# inews_url = "https://inews.co.uk/"
-# morningpost_url = "https://morningstaronline.co.uk/"
-# dailystar_url = "https://www.dailystar.co.uk/"
-# sun_url = "https://www.thesun.co.uk/"
-# eveningstandard_url = "https://www.standard.co.uk/"
-# irishsun_url = "https://www.thesun.ie/"
-# thescotsman = "https://www.scotsman.com/"
-# metro_url = "https://metro.co.uk/"
-# herald_url = "https://www.heraldscotland.com/"
+inews_url = "https://inews.co.uk/"
+morningstar_url = "https://morningstaronline.co.uk/"
+dailystar_url = "https://www.dailystar.co.uk/"
+sun_url = "https://www.thesun.co.uk/"
+eveningstandard_url = "https://www.standard.co.uk/"
+irishsun_url = "https://www.thesun.ie/"
+thescotsman_url = "https://www.scotsman.com/"
+metro_url = "https://metro.co.uk/"
+# herald_url = "https://www.heraldscotland.com/".
+irishtimes_url = "https://www.irishtimes.com/"
 
 headlines = []
 
@@ -130,6 +139,78 @@ def scrape(url):
     else:
       headline = "Error - failed to scrape the " + paper
 
+  if url == metro_url:
+    paper = "Metro"
+    headline_html = soup.find('span', class_="colour-box")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == dailystar_url:
+    paper = "The Daily Star"
+    headline_html = soup.find('a', class_="publication-font")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == inews_url:
+    paper = "The i"
+    headline_html = soup.find('a', class_="article-title sc-cHSUfg fcmytr")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == sun_url:
+    paper = "The Sun"
+    headline_html = soup.find('p', class_="teaser__subdeck")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == morningstar_url:
+    paper = "The Morning Star"
+    headline_html = soup.find('div', class_="top-story")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == eveningstandard_url:
+    paper = "The Evening Standard"
+    headline_html = soup.find('div', class_="content")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == irishsun_url:
+    paper = "The Irish Sun"
+    headline_html = soup.find('p', class_="teaser__subdeck")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+  
+  if url == thescotsman_url:
+    paper = "The Scotsman"
+    headline_html = soup.find('a', class_="article-title sc-cHSUfg fcmytr")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
+  if url == irishtimes_url:
+    paper = "The Irish Times"
+    headline_html = soup.find('span', class_="h2")
+    if headline_html != None:
+      headline = headline_html.text.strip()
+    else:
+      headline = "Error - failed to scrape the " + paper
+
   # head = Headline(paper, headline)
   time_stamp = datetime.datetime.now()
   date_stamp = time_stamp.strftime("%H:%M:%S (%Y-%m-%d)")
@@ -138,6 +219,7 @@ def scrape(url):
   headlines.append(myDictObj)
 
 def print_headlines():
+  # clear the list so we only get the latest headlines
   headlines.clear()
   for url in urls.values():
     scrape(url)
