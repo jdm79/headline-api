c642dd922432152d5ab8dd5e87a0c5149a33a2da
jdm79 <james.malvern@gmail.com>
2020-08-22 23:21:18 +0200

    refactor the code a bit, use different files

---
+++ urls.py
@@ -0,0 +1,21 @@
+urls = {
+  "dailymirror_url":"https://www.mirror.co.uk/",
+  "independent_url": "https://www.independent.co.uk/news/uk",
+  "times_url": "https://www.thetimes.co.uk/",
+  "dailymail_url": "https://www.dailymail.co.uk/home/index.html",
+
+  "guardian_url": "https://www.theguardian.com/uk", 
+  "financialtimes_url": "https://www.ft.com/",
+  "eveningstandard_url": "https://www.standard.co.uk/",
+  "dailyexpress_url": "https://www.express.co.uk/",
+
+  "inews_url": "https://inews.co.uk/",
+  "metro_url": "https://metro.co.uk/",
+  "dailystar_url": "https://www.dailystar.co.uk/",
+  "sun_url": "https://www.thesun.co.uk/",
+
+  "morningstar_url": "https://morningstaronline.co.uk/",
+  "irishsun_url": "https://www.thesun.ie/",
+  "thescotsman_url": "https://www.scotsman.com/news/uk-news/",
+  "telegraph_url": "https://www.telegraph.co.uk/",
+}
